67ee3e5b5fd754ba13a458574ef570e072f79f81
chhavi <60265068+chhavi5496@users.noreply.github.com>
2020-01-26 13:17:23 +0530

    Add files via upload

---
+++ constructor.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class num
+{
+int a,b,c,d;
+num()
+{ 
+a=1;b=2;c=3;d=4;
+r=a+c;
+s=b+d;
+System.out.println(r"+i"+s);
+}
+}
+public class constructor
+{
+public static void main(String args[])
+{
+int x,y;
+Scanner sc=new Scanner(System.in);
+x=sc.nextInt();
+Scanner sc=new Scanner(System.in);
+y=sc.nextInt();
+num ob=new num();
+num ob1=new num();
+num ob2=new num();
+num ob3=ob1;
+}
+}
+
+
